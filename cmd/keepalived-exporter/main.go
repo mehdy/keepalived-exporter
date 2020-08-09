eb99e1a79391b69286e8ede266c865713a0500cd
Yashar Nesabian <nesabian@gmail.com>
2020-08-09 10:04:13 +0430

    fix: fixed a type in --help argument

--- cmd/keepalived-exporter/main.go
+++ cmd/keepalived-exporter/main.go
@@ -23,7 +23,7 @@ func main() {
 	metricsPath := flag.String("web.telemetry-path", "/metrics", "A path under which to expose metrics.")
 	keepalivedJSON := flag.Bool("ka.json", false, "Send SIGJSON and decode JSON file instead of parsing text files.")
 	keepalivedPID := flag.String("ka.pid-path", "/var/run/keepalived.pid", "A path for Keepalived PID")
-	keepalivedCheckScript := flag.String("cs", "", "Health Cehck script path to be execute for each VIP")
+	keepalivedCheckScript := flag.String("cs", "", "Health Check script path to be execute for each VIP")
 	versionFlag := flag.Bool("version", false, "Show the current keepalived exporter version")
 
 	flag.Parse()
