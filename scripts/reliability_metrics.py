571a5017c528d84f6637e3ca35a50b3e92db18b8
Ronnie Adato <ronniea@ottopia.tech>
2023-04-30 13:55:21 +0300

    connected to logstash

--- scripts/reliability_metrics.py
+++ scripts/reliability_metrics.py
@@ -1,18 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
 import datetime
 import os
 import logging
 import threading
 import queue
 import re
+from ottopia_logging.logging_factory import LoggingFactory
+from ottopia_logging.log_level import LogLevel
+from ottopia_logging.logging_settings import LoggingSettings
 
-#from ottopia_logging.logging_factory import LoggingFactory
-#from settings import settings
 
-#logger = LoggingFactory.get_logger(
-#    module_name=__name__,
-#    logging_settings=settings.logging_settings,
-#)
+class LoggerSetting(LoggingSettings):
+    """
+    Logger settings
+    """
+
+    DEFAULT_COMPONENT_NAME: str = "keepalived-exporter"
+
+    DEFAULT_LOG_LEVEL: LogLevel = LogLevel.INFO
+
+    class Config:
+        """
+        Config for Logger settings
+        """
+
+        arbitrary_types_allowed = True
+
 
 
 class LineData:
@@ -33,9 +46,14 @@ class Service:
         while not self.event.is_set() or not self.queue.empty():
             line_data: LineData = self.queue.get()
             if line_data and fail_line in line_data.line:
-                logger = logging.getLogger(f'{self.name}')
-                logger.info(f'found on {line_data.syslog_date}: {fail_line}')
-                # TODO: send to logstash
+                self.report_to_logstash(line_data.syslog_date, fail_line)
+
+    def report_to_logstash(self, syslog_date: datetime.datetime, fail_line: str):
+        logstash_logger = LoggingFactory.get_logger(
+            module_name=self.name,
+            logging_settings=LoggerSetting(),
+        )
+        logstash_logger.info(f'service {self.name} failed on {syslog_date}: {fail_line}')
 
     def start(self):
         logger = logging.getLogger(f'{self.name}')
@@ -51,84 +69,90 @@ class Service:
             self.queue.put(None)
         self.thread.join()
 
-    def put(self, line_data: LineData):
+    def async_analyze(self, line_data: LineData):
         logger = logging.getLogger(f'{self.name}')
         logger.debug('putting line on queue')
         self.queue.put(line_data)
 
 
-def put_chunk_lines_on_queue(chunk: str, services: list[Service], current_time: datetime.datetime, regex: re.Pattern[str]) -> bool:
-    '''
-    put lines from chunk on queue, return True if chunk is in range, False otherwise
-    '''
-
-    logger = logging.getLogger(__name__)
-    THRESHOLD_SECONDS: datetime.timedelta = datetime.timedelta(seconds=1000000)
-    lines: list[str] = reversed(chunk.split('\n'))
-    continue_flag: bool = True
-    for line in lines:
-        date_str: str = line[: 15]
-        try:
-            if not regex.match(date_str):
-                continue
-
-            syslog_date: datetime.datetime = datetime.datetime.strptime(date_str, '%b %d %H:%M:%S').replace(year=current_time.year)
-            continue_flag &= syslog_date + THRESHOLD_SECONDS >= current_time
+class SyslogParser:
+    SYSLOG_PATH: str = '/var/log/syslog'
+    def __init__(self, services: list[Service]):
+        self.services: list[Service] = services
+        self.current_time: datetime.datetime = datetime.datetime.now()
+        # date format is: 'MMM dd HH:MM:SS'
+        self.regex: re.Pattern[str] = re.compile(r'(\w{3} \d{2} \d{2}:\d{2}:\d{2})')
 
 
-            if continue_flag:
-                line_data: LineData = LineData(line[17:], syslog_date)
-                for service in services:
-                    service.put(line_data)
-            else:
-                logger.debug(f'{line} not in range, {current_time}')
-                break
 
-        except ValueError:
-            continue_flag = False
+    def put_chunk_lines_on_queue(self, chunk: str) -> bool:
+        '''
+        put lines from chunk on queue, return True if chunk is in range, False otherwise
+        '''
 
-    return continue_flag
-
-
-def search_syslog_for_errors_in_services(services: list[Service]) -> int:
-    '''
-    open syslog file
-    search for services errors from the back of the file
-    and don't look past the last reboot and the last 10 seconds
-    return 0 if no errors found
-    return 1 if errors found
-    return 2 if syslog file not found
-    '''
-    current_time: datetime.datetime = datetime.datetime.now()
-    if not os.path.exists("/var/log/syslog"):
-        return 2
-
-    # date format is: 'MMM dd HH:MM:SS'
-    regex: re.Pattern[str] = re.compile(r'(\w{3} \d{2} \d{2}:\d{2}:\d{2})')
-    MAX_CHUNK_SIZE = 0x1000
-    try:
-        prev_chunk_remainder: str = ''
-        with open("/var/log/syslog", "r") as syslog:
-            syslog.seek(0, os.SEEK_END) #go to end of file
-            continue_flag: bool = True
-            while continue_flag and syslog.tell() > 0:
-                chunk_size = min(syslog.tell(), MAX_CHUNK_SIZE)
-                interval: int = syslog.tell() - chunk_size
-                syslog.seek(interval, os.SEEK_SET)
-                chunk: str = syslog.read(chunk_size) + prev_chunk_remainder
-                syslog.seek(syslog.tell() - chunk_size, os.SEEK_SET)
-
-                pos = chunk.find('\n')
-                prev_chunk_remainder = chunk[:pos]
-
-                continue_flag = put_chunk_lines_on_queue(chunk[pos + 1:], services, current_time, regex)
-
-    except Exception as e:
         logger = logging.getLogger(__name__)
-        logger.exception(e)
-        return 1
+        THRESHOLD_SECONDS: datetime.timedelta = datetime.timedelta(seconds=5)
+        lines: reversed[str] = reversed(chunk.split('\n'))
+        continue_flag: bool = True
+        for line in lines:
+            date_str: str = line[: 15]
+            try:
+                if not self.regex.match(date_str):
+                    continue
 
-    return 0
+                # date format is: 'MMM dd HH:MM:SS'
+                syslog_date: datetime.datetime = datetime.datetime.strptime(date_str, '%b %d %H:%M:%S').replace(year=self.current_time.year)
+                continue_flag &= syslog_date + THRESHOLD_SECONDS >= self.current_time
+
+
+                if continue_flag:
+                    line_data: LineData = LineData(line[17:], syslog_date)
+                    for service in self.services:
+                        service.async_analyze(line_data)
+                else:
+                    logger.debug(f'{line} not in range, {self.current_time}')
+                    break
+
+            except ValueError:
+                continue_flag = False
+
+        return continue_flag
+
+    def search_for_services_errors(self) -> int:
+        '''
+        open syslog file
+        search for services errors from the back of the file
+        and don't look past the last reboot and the last 10 seconds
+        return 0 if no errors found
+        return 1 if errors found
+        return 2 if syslog file not found
+        '''
+        if not os.path.exists(self.SYSLOG_PATH):
+            return 2
+
+        MAX_CHUNK_SIZE = 0x1000
+        try:
+            prev_chunk_remainder: str = ''
+            with open(self.SYSLOG_PATH, 'r') as syslog:
+                syslog.seek(0, os.SEEK_END) #go to end of file
+                continue_flag: bool = True
+                while continue_flag and syslog.tell() > 0:
+                    chunk_size = min(syslog.tell(), MAX_CHUNK_SIZE)
+                    syslog.seek(syslog.tell() - chunk_size, os.SEEK_SET)
+                    chunk: str = syslog.read(chunk_size) + prev_chunk_remainder
+                    syslog.seek(syslog.tell() - chunk_size, os.SEEK_SET)
+
+                    pos = chunk.find('\n')
+                    prev_chunk_remainder = chunk[:pos]
+
+                    continue_flag = self.put_chunk_lines_on_queue(chunk[pos + 1:])
+
+        except Exception as e:
+            logger = logging.getLogger(__name__)
+            logger.exception(e)
+            return 1
+
+        return 0
 
 
 def init_logging():
@@ -141,14 +165,14 @@ def init_logging():
 def main() -> int:
     init_logging()
 
-    SERVICE_NAMES = ('tca', 'relayserver')
     event: threading.Event = threading.Event()
-    services: list[Service] = []
-    for service_name in SERVICE_NAMES:
-        service: Service = Service(service_name, event)
+    SERVICE_NAMES: list[str] = ['tca', 'relayserver']
+    services: list[Service] = [Service(service_name, event) for service_name in SERVICE_NAMES]
+    for service in services:
         service.start()
-        services.append(service)
-    ret_val: int = search_syslog_for_errors_in_services(services)
+
+    syslog_parser: SyslogParser = SyslogParser(services)
+    ret_val: int = syslog_parser.search_for_services_errors()
 
     event.set()
     for service in services:
@@ -161,9 +185,10 @@ def main() -> int:
 
 import unittest
 # write unit tests for each function in this module
-def test_search_syslog_for_errors_in_services():
+def test_search_for_services_errors():
     pass
 
 
 if __name__ == "__main__":
     exit(main())
+
